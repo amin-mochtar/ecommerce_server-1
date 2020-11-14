d71f086d140e8eb968052a834232671faeacfc27
aminmochtarclear <aminmr.mochtar0123@gmail.com>
2020-11-14 10:53:36 +0700

    push heroku cors

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const router = require('./routes')
 const errorHandle = require("./middlewares/errorHandle.js");
+const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-
+app.use(cors())
 app.use(router)
 app.use(errorHandle)
 
